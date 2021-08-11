ca6983aebc770469afb62969de85f7cc72818f5f
Felipe Santana <felipe008_@hotmail.com>
2021-08-11 12:44:13 -0300

    feature: Personalização das listas dos títulos (nao finalizado)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, {useEffect, useState} from 'react';
 import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow.js'
+import FeaturedMovie from './components/FeaturedMovie';
+
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState([]);
 
   useEffect(()=>{
   const loadAll = async () => {
@@ -19,6 +22,11 @@ export default () => {
 
   return (
 <div className="page">
+
+    {featuredData && 
+    <FeaturedMovie item={featuredData} />
+    }
+
   <section className="lists">
     {movieList.map((item, key) => (
       <div>
