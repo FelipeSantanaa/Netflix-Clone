3a808145bcc26f5de2680e409fa939aba111ef5d
Felipe Santana <felipe008_@hotmail.com>
2021-08-11 10:47:06 -0300

    feature: Adicionada API do TMDB

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
+import Tmdb from './Tmdb';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+useEffect(()=>{
+  const loadAll = async () => {
+    //lista total
+    let list = await Tmdb.getHomeList();
+    console.log(list);
+    setMovieList(list);
+  }
+  loadAll();
+}, []);
+
   return (
-    <div>
-      Olá mundo!
-    </div>
+<div className="page">
+  
+</div>
 
   );
 }
